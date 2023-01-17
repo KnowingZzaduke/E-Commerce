06cc2dfeeb61bcd39d90987323a90a3cd8adbf84
KnowingZzaduke <josex.developer5013@gmail.com>
2023-01-16 20:40:49 -0500

    Archivos y estructura base del E-commerce

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const {src, dest, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+
+function css(done){
+    src("src/scss/**/*.scss").pipe(plumber()).pipe(sass()).pipe(dest("build/css"));
+    done();
+}
+
+function dev(done){
+    watch("src/**/*.scss", css);
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
